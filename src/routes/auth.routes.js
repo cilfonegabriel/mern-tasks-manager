ba77fbba3fe7d8324ea30b8279f7373c656d11f8
gabicilfone <cilfonegabriel@gmail.com>
2023-08-02 11:50:33 -0300

    add controller on routers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { login, register } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post('/register')
-router.post('/login')
+router.post('/register', register);
+
+router.post('/login', login);
