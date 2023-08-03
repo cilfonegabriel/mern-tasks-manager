2e16a3c1b650281f5cbbce57628a737146bcac5a
gabicilfone <cilfonegabriel@gmail.com>
2023-08-03 07:20:08 -0300

    add a connection to the mongo database

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import express from "express";
-import morgan from "morgan";
+import express from 'express';
+import morgan from 'morgan';
 
 import authRoutes from './routes/auth.routes.js';
-import router from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
 
-app.use("/api",authRoutes);
+app.use('/api', authRoutes);
 
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,20 @@
-export const register = (req,res) => res.send('register');
+import User from '../models/user.model.js';
+
+export const register = async (req, res) => {
+    const { email, password, username } = req.body;
+
+    try {
+        const newUser = new User({
+            email,
+            password,
+            username,
+        });
+        
+        await newUser.save();
+        res.send('registration');
+    }catch(error) {
+     console.log(error);
+    }
+};
 
 export const login = (req,res) => res.send('login');
